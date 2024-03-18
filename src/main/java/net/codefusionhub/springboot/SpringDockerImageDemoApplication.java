65f4538474ec6f6ad49f94bb3b9178f51c1f7d42
Krishna Ghodke <krushnaghodke90@gmail.com>
2024-03-18 08:13:51 -0700

    Initial commit

---
+++ src/main/java/net/codefusionhub/springboot/SpringDockerImageDemoApplication.java
@@ -0,0 +1,13 @@
+package net.codefusionhub.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerImageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerImageDemoApplication.class, args);
+	}
+
+}
